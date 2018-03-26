2e509ac525e1cfe6b84f11f7f53e967904eacb52
pestovpvl <pestovpvl@mail.ru>
2018-03-25 22:34:32 -0400

    Add Rakefile, upadate Gemfile, add migration

--- app.rb
+++ app.rb
@@ -1,6 +1,19 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/activerecord'
+
+
+set :database, "sqlite3:pizzashop.db"
+
+class Product < ActiveRecord::Base
+
+end
+
+
+
+
+
 
 configure do
   enable :sessions
