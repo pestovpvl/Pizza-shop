0cfe5f651e737974f5f75114cc55a6e9391887db
pestovpvl <pestovpvl@mail.ru>
2018-03-26 21:27:11 -0400

    Add stub add to cart button

--- public/app.js
+++ public/app.js
@@ -1,10 +1,3 @@
-function  something() {
-
-
-
-    var x = window.localStorage.getItem('bbb');
-    x = x * 1 + 1;
-    window.localStorage.setItem('bbb', x);
-
-    alert(x);
+function add_to_cart() {
+    alert("Hello!");
 }
