91079644cd7fa89bf7a284b53a1d441d3bdb3498
pestovpvl <pestovpvl@mail.ru>
2018-03-28 22:05:49 -0400

    Add cart on right nav

--- public/app.js
+++ public/app.js
@@ -5,7 +5,7 @@ function add_to_cart(id) {
     var x = window.localStorage.getItem(key);
     x = x * 1 + 1;
     window.localStorage.setItem(key, x);
-    
+
     alert("Item in your cart: " + cart_get_number_of_items());
 }
 
